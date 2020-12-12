3ebb83a00959f36a2be9875632c5551fa4e7cd84
irishernandezper <74354006+irishernandezper@users.noreply.github.com>
2020-12-12 16:43:24 +0000

    committing proyect

---
+++ download.R
@@ -0,0 +1,10 @@
+filename<- "Final.zip"
+
+if(!file.exists(filename)){
+        fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+        download.file(fileURL,filename)
+}
+if(!file.exists("UCI HAR Dataset")) {
+        unzip(filename)
+}
+
